1df565d299a0cd0cf7a03c690eecda2f53685851
WENBBBY <145891074+WENBBBY@users.noreply.github.com>
2024-08-04 13:25:50 +0800

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+//test
+//test_dony
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    // 乘法
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+    // hello world
+    public String greeting() {
+        return "Hello WENBY";
+    }
+}
